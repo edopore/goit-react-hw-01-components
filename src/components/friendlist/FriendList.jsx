1871751b5b73c83f8f67b1353393d086f459c610
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-03-14 17:22:51 -0500

    friendlist done

--- src/components/friendlist/FriendList.jsx
+++ src/components/friendlist/FriendList.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
-
+import FriendListItem from './FriendListItem';
+import './friendList.css';
 export default function FriendList(props) {
-  return <div>FriendList</div>;
+  return (
+    <div>
+      <ul className="friends-list m-0 p-0">
+        {props.friends.map((friend, index) => (
+          <FriendListItem friend={friend} key={index} />
+        ))}
+      </ul>
+    </div>
+  );
 }
